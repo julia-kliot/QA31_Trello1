1fd47ec34d24899f0d6256b2876b75b6d3d266a2
julia-kliot <juliakliot.jk@gmail.com>
2022-01-09 20:39:39 +0300

    login+logout

--- src/test/java/Tests_Trello/Login.java
+++ src/test/java/Tests_Trello/Login.java
@@ -3,11 +3,12 @@ package Tests_Trello;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class Login {
     WebDriver wd;
@@ -18,30 +19,62 @@ public class Login {
         WebDriverManager.chromedriver().setup();
         wd = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "C:/Users/julia/Documents/QA/QA_Automation/QA/QA31_Trello1/chromedriver.exe");
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wd.navigate().to("https://trello.com/");
+
     }
+
 
     @Test
-    public void  loginTest1() throws InterruptedException {
-        WebElement signup = wd.findElement(By.cssSelector("[href='/login']"));
-        signup.click();
-        WebElement email= wd.findElement(By.cssSelector("#user"));
-        type(email, "juliakliot.jk@gmail.com");
-        WebElement login= wd.findElement(By.cssSelector("#login"));
-        login.click();
-        //
-        WebElement password= wd.findElement(By.cssSelector("#password"));
-        type(password,"misha240613");
-        WebElement loginSubmit= wd.findElement(By.cssSelector("#login-submit"));
-        loginSubmit.click();
-        Thread.sleep(5000);
+    public void loginTest1() {
+//initLogin();
+        click(By.cssSelector("[href='/login']"));
+        //enterUserName();
+        type(By.cssSelector("#user"), "juliakliot.jk@gmail.com");
+        click(By.cssSelector("#login"));
+        //enterPassword();
+        type(By.cssSelector("#password"),"misha240613");
+        //confirmLogin();
+        click(By.cssSelector("#login-submit"));
+
+
+    }
+    @Test
+    public void loginTest2() {
+
+        click(By.cssSelector("[href='/login']"));
+        type(By.cssSelector("#user"), "juliakliot.jk@gmail.com");
+        click(By.cssSelector("#login"));
+
+        click(By.cssSelector(".dlMUdg"));
+        type(By.cssSelector("#password"),"misha240613");
+        click(By.cssSelector("#login-submit"));
+
 
     }
 
-    private void type(WebElement email, String myemail) {
-        email.click();
-        email.clear();
-        email.sendKeys(myemail);
+    public void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
     }
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    public void logout(){
+        click(By.cssSelector("[data-test-id='header-member-menu-button'"));
+        click(By.cssSelector("[data-test-id='header-member-menu-logout']"));
+        click(By.cssSelector("[data-testid='logout-button']"));
+
+    }
+
+    public boolean isLogged(){
+        return  wd.findElements(By.cssSelector("._2ft40Nx3NZII2i")).size()>0;
+    }
+
+
 
 }
